65cfef31044eeae3fcfb25316056c65ab7f1a0df
Erick <erick.hx22@gmail.com>
2019-10-29 11:13:39 -0500

    Queries acabado

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 /* Manejo del DOM */
+
+var propValores = {
+    genero: document.getElementById('genero').value,
+    rol: document.getElementById('rol').value,
+    casa: document.getElementById('casa').value
+}
+var metValores = {
+    inicio: function () {
+        console.log(propValores.genero)
+
+    }
+}
+
+
+function genero() {
+    let gender = document.getElementById('genero').value
+    console.log(gender)
+
+}
